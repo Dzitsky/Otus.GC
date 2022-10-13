6126edb505a73b7f814cf38ae6860bb2d2001745
Automerge <automerge@rts-tender.ru>
2022-10-13 21:49:06 +0300

    Add project files.

---
+++ GC/TrimerExample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+
+public class TrimerExample
+{
+	public void Start()
+	{
+		Timer trimer = new Timer(obj => Console.WriteLine(DateTime.Now), null, 0, 200);
+		Console.WriteLine("Hello World!");
+
+        GC.Collect();
+	}
+}
